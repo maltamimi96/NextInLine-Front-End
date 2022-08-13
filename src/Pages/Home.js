fcbeceaaa9e8187ec055a92325e93c0a5763bc57
rayves <vo.raymond.v@outlook.com>
2022-08-13 14:29:33 +1000

    Home Page - refactors image styles | adds images

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,30 +1,46 @@
-import Main from "../Components/Main/Main";
-import { Button, Card, CardContent, Typography } from "@mui/material";
-import { Box, height } from "@mui/system";
-import StoreNav from "../Components/Nav/StoreNav";
+import Main from '../Components/Main/Main';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import { Box, height } from '@mui/system';
+import StoreNav from '../Components/Nav/StoreNav';
+import barberTrim from '../images/barber-trim.jpg';
+import barberSeats from '../images/barber-seats.jpg';
+import barberTools from '../images/barber-tools.jpg';
+import barberTools2 from '../images/barber-tools2.jpg';
+
+//Styles
+const styleBoxImage = {
+  borderStyle: 'solid',
+  width: '200px',
+  height: '200px',
+  objectFit: 'cover',
+};
+
 function Home() {
   return (
     <>
-    <section className="jumbotron">
-    <Main></Main>
-    </section>
-    <section className="banner theme--color" >
-      <Box sx={{display:'flex' ,justifyContent:'center',gap:10}}>
-        <Box sx={{borderStyle:'solid' ,width:'200px',height:'200px'}}></Box>
-        <Box sx={{borderStyle:'solid' ,width:'200px',height:'200px'}}></Box>
-        <Box sx={{borderStyle:'solid' ,width:'200px',height:'200px'}}></Box>
-        <Box sx={{borderStyle:'solid' ,width:'200px',height:'200px'}}></Box>
-      </Box>
-    </section>
-    <section className="banner theme--color3" >
-      <Box sx={{display:'flex',justifyContent:'space-between'}}>
-        <Typography variant="h6">Easy To Implement in any Salon and hair Dresser </Typography>
-        <Button sx={{backgroundColor:'black'}} variant="contained">View How</Button>
-      
-      </Box>
+      <section className="jumbotron">
+        <Main></Main>
       </section>
-      </>
-  )
+      <section className="banner theme--color">
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 10 }}>
+          <Box component="img" src={barberTrim} sx={styleBoxImage}></Box>
+          <Box component="img" src={barberSeats} sx={styleBoxImage}></Box>
+          <Box component="img" src={barberTools} sx={styleBoxImage}></Box>
+          <Box component="img" src={barberTools2} sx={styleBoxImage}></Box>
+        </Box>
+      </section>
+      <section className="banner theme--color3">
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="h6">
+            Easy To Implement in any Salon and hair Dresser{' '}
+          </Typography>
+          <Button sx={{ backgroundColor: 'black' }} variant="contained">
+            View How
+          </Button>
+        </Box>
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
